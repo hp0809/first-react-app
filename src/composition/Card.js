51480ecd2622a9c11d161f5ce8297e14be3d73d4
Hali Power <hpower0809@gmail.com>
2019-05-27 18:23:41 +0100

    updated files for event handlers

--- src/composition/Card.js
+++ src/composition/Card.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './Card.css';
-var store= require ('./store.js');
 
 
+/* This was what I did, the following is copied from the bloc repo
 function Card(props) {
     return (
         <div>
@@ -16,7 +16,27 @@ function Card(props) {
                 )
             }
             )}
+            <button className='List-add-button'>+ Add Random Card</button>
         </div>
     );
 }
 export default Card;
+*/
+
+export default function Card(props) {
+    return (
+      <div className='Card'>
+        <button
+          type='button'
+          onClick={() => props.onClickDelete(props.id)}
+        >
+          delete
+        </button>
+        <h3>{props.title}</h3>
+        <p>{props.content}</p>
+      </div>
+    )
+  }
+  Card.propTypes = {
+    onClickDelete: () => {}
+  }
--- src/composition/List.js
+++ src/composition/List.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Card from './Card.js';
 import './List.css';
-var store= require ('./store.js');
 
 
+/* This was what I did, the following is copied from the bloc repo
 function List(header, cards) {
     return (
         <>
@@ -14,7 +14,7 @@ function List(header, cards) {
                             <h2>{card.header}</h2>
                         </header>
                         <div className="List-cards">
-                            <Card cardIds={card.cardIds} />
+                            <Card cardIds={card.cardIds} key={card.id} />
                         </div>
                         
                     </section> 
@@ -26,3 +26,37 @@ function List(header, cards) {
 }
 
 export default List;
+
+*/
+
+export default function List(props) {
+    return (
+      <section className='List'>
+        <header className='List-header'>
+          <h2>{props.header}</h2>
+        </header>
+        <div className='List-cards'>
+          {props.cards.map((card) =>
+            <Card
+              key={card.id}
+              id={card.id}
+              title={card.title}
+              content={card.content}
+              onClickDelete={props.onClickDelete}
+            />
+          )}
+          <button
+            type='button'
+            className='List-add-button'
+            onClick={() => props.onClickAdd(props.id)}
+          >
+            + Add Random Card
+          </button>
+        </div>
+      </section>
+    )
+  }
+
+  List.defaultProps = {
+    onClickAdd: () => {},
+  }
